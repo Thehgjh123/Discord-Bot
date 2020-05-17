47ca40f72262a6a819d813e45673c20ddd983df9
Thehgjh123 <65503268+Thehgjh123@users.noreply.github.com>
2020-05-17 11:56:49 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NzExMzQwMDU5NDg3NjMzNTgx.XsBlQQ.WGkSZEbtWA6Dg-xjzlR-UU2iaNA';
+
+const PREFIX = '$';
+
+bot.on('ready', () =>{
+    console.log('THIS BOT IS ONLINE!');
+})
+bot.on('message', message=>{
+    
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            message.channel.send('Pong!');
+            break;
+        case 'Youtube':
+            message.channel.send('https://www.youtube.com/channel/UCGw_svU6fZ_zA0uIO7n_zDQ');
+            break;
+        case 'TeccyBot':
+            message.channel.send('You Can Invite Me To Your Server Using This Link: https://discordapp.com/oauth2/authorize?client_id=711340059487633581&scope=bot&permissions=8');
+            break;
+        case 'clear':
+            if(!args[1]) return message.reply('Command Incomplete')
+            message.channel.bulkDelete(args[1]);
+            break;
+    }
+})
+
+client.login(process.env.BOT_TOKEN);
